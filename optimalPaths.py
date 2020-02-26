a08a5f60f1a75ad8be4a305e241036f59fd21bd8
lets-get-this-sandwich <61244051+lets-get-this-sandwich@users.noreply.github.com>
2020-02-26 11:15:29 -0500

    Update optimalPaths.py

--- optimalPaths.py
+++ optimalPaths.py
@@ -18,7 +18,29 @@ def optimalPaths(n,a,b):
         newPaths = []
         while len(paths):
             path=paths.pop()
+<<<<<<< HEAD:optimalPaths.py
             addcity(path,cities,mincost,n)
+=======
+            for city in cities[path[-1]]:
+                    if city not in path:
+                        if path[-1]!=1 or sneakers(city,path,cities):
+                            newPath = path + [city]
+                            newPath[0] += LCMdict[newPath[-1]][newPath[-2]]
+                            newPath[0]-=newPath[-1]
+                            if newPath[0] <= mincost:
+                                tempPaths.append(newPath)
+                                if sos(newPath,n):
+                                    mincost = newPath[0]
+                    elif city==1 and path[-1]!=1:
+                        newPath = path + [city]
+                        newPath[0] += newPath[-2]
+                        newPath=simplify(newPath)
+                        if not (newPath in tempPaths):
+                            if newPath[0] <= mincost:
+                                tempPaths.append(newPath)
+                                if sos(newPath,n):
+                                    mincost = newPath[0]
+>>>>>>> parent of cc45d8b... Update optimalPaths:optimalPaths
         for path in tempPaths:
             if path[0]<=mincost:
                 if path.count(1)<b:
